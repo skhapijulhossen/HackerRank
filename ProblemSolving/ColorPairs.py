fd9cfdd8b9dc60ee706d3335db957c24f7360238
SK HAPIJUL HOSSEN <skhapijulhossen@gmail.com>
2021-06-12 20:47:51 +0530

    3 Problems Solved

--- ProblemSolving/ColorPairs.py
+++ ProblemSolving/ColorPairs.py
@@ -5,15 +5,3 @@ def sockMerchant(n, ar):
         counter += (ar.count(color) // 2)
         
     return counter
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
